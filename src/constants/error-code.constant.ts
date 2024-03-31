3ee418b37b16b647be30d5b7117cabb35ffd216a
Loc Nguyen <locnguyen071102@gmail.com>
2024-03-31 23:02:59 +0700

    Update: Register account response

--- src/constants/error-code.constant.ts
+++ src/constants/error-code.constant.ts
@@ -3,7 +3,7 @@ export enum ErrorEnum {
     PASSWORD_VERIFICATION_FAILED = "404:Confirmation password does not match",
     USER_IS_BLOCKED = "403:User is blocked",
     USER_NOT_FOUND = "404:User not found",
-    USER_INVALID = "403:User not found or blocked",
+    USER_INVALID = "403:Confirm your email first, user not found or blocked",
     USER_UNCONFIRMED = "401:Confirmation your email before!",
     INVALID_LOGIN = "404:Invalid login, please log in again",
     NO_PERMISSION = "403:Access was denied",
