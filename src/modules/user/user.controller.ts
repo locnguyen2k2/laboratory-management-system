8ecae93c2c43eaac4fa72ebc6d0084b406ebc4d2
Loc Nguyen <locnguyen071102@gmail.com>
2024-04-03 21:24:44 +0700

    Fix: Base Dto

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -102,6 +102,7 @@ export class UserController {
     @Post('forget-password')
     async resetPassword(@Body() dto: ResetPaswordDto): Promise<any> {
         const data = ResetPaswordDto.plainToClass(dto);
+        console.log(data);
         return await this.userService.resetPassword(data.email);
     }
 
