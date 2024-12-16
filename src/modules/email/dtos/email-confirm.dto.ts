66a124a091d7013192393a34d0c2bf82c8c67ebf
locnguyen <locnguyen071102@gmail.com>
2024-12-16 23:50:06 +0700

    refector: send mail for portfolio

--- src/modules/email/dtos/email-confirm.dto.ts
+++ src/modules/email/dtos/email-confirm.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class EmailLinkConfirmDto {
@@ -8,4 +8,22 @@ export class EmailLinkConfirmDto {
   @IsNotEmpty()
   @Expose()
   email: string;
+}
+
+export class EmailFeedBackDto {
+  @ApiProperty({ default: '' })
+  @IsEmail()
+  @IsNotEmpty()
+  @Expose()
+  from: string;
+
+  @ApiProperty({ default: '' })
+  @IsNotEmpty()
+  @Expose()
+  content: string;
+
+  @ApiProperty({ default: '' })
+  @IsNotEmpty()
+  @Expose()
+  key: string;
 }
--- src/modules/email/mail.controller.ts
+++ src/modules/email/mail.controller.ts
@@ -1,6 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { EmailFeedBackDto } from './dtos/email-confirm.dto';
+import { MailService } from './mail.service';
+import { env } from '../../global/env';
 
-@Controller('mailer')
+@Controller()
 export class MailController {
-  constructor() {}
+  constructor(private readonly mailService: MailService) {}
+
+  @Post('feed-back')
+  async sendFeedBackForPortfolio(@Body() data: EmailFeedBackDto) {
+    if (data.key !== env('EMAIL_PASSWORD')) {
+      throw new HttpException(`Access was denied!`, HttpStatus.BAD_REQUEST);
+    }
+
+    await this.mailService.sendEmail({
+      to: 'locnguyen071102@gmail.com',
+      subject: `Phản hồi từ: ${data.from}`,
+      text: data.content,
+    });
+
+    return 'Gui thanh cong!';
+  }
 }
